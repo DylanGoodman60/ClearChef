2437bdf243be7ebf595ad405b5fce4216428b32e
Taylor Newman <newmant@uoguelph.ca>
2024-04-04 14:21:57 -0400

    Working viewer

---
+++ ClearChef/Models/DataStore.swift
@@ -0,0 +1,48 @@
+//
+//  RecipeStore.swift
+//  ClearChef
+//
+//  Created by Taylor Newman on 2024-04-03.
+//
+
+import SwiftUI
+
+class DataStore: ObservableObject {
+    @Published var recipes: [Recipe] = [
+        .init(title: "Pad Thai",
+              description: "Easy Pad Thai Recipe",
+              category: Category(title: "Breakfast"),
+              cookTime: ["1 h", "10 m"],
+              ingredients: [
+                Ingredient(title: "400 g Rice Noodles"),
+                Ingredient(title: "1/2 tbsp Soy Sauce"),
+                Ingredient(title: "3 Lime Wedges")
+              ],
+              directions: [
+                Direction(title: "Cook Rice Noodles"),
+                Direction(title: "Add soy sauce"),
+                Direction(title: "Eat")
+              ],
+              image: Image("padThai")),
+        .init(title: "OatMeal",
+              description: "Easy Oatmeal Recipe",
+              category: Category(title: "Breakfast"),
+              cookTime: ["0 h", "10 m"],
+              ingredients: [
+                Ingredient(title: "1/2 cup rolled oats"),
+                Ingredient(title: "1/2 tbsp suager"),
+                Ingredient(title: "Blueberries")
+              ],
+              directions: [
+                Direction(title: "Cook Oats"),
+                Direction(title: "Add berries"),
+                Direction(title: "Eat")
+              ],
+              image: Image("oatmeal")),
+    ]
+    
+/// We can use this later to get recipes from storage
+//    init(isDarkMode: Bool) {
+//        self._recipes = Published(initialValue: functionCall())
+//    }
+}
---
+++ ClearChef/Views/RatingView.swift
@@ -0,0 +1,8 @@
+//
+//  RatingView.swift
+//  ClearChef
+//
+//  Created by Taylor Newman on 2024-04-03.
+//
+
+import Foundation
